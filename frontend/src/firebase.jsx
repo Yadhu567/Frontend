9c009f169416844deeeaa79ffa27ef4d53d10f3a
Yadhu567 <84367906+Yadhu567@users.noreply.github.com>
2024-05-10 18:24:00 +0530

    Add files via upload

---
+++ frontend/src/firebase.jsx
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+
+
+
+
